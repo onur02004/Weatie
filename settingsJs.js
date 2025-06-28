1a72e0b9e795d5d8cf99496bb5fa99c2c66c7ad4
Maja Schweier <maja@schweier.org>
2025-06-28 18:02:32 +0200

    Änderungen an Settings + Zentrierung

--- settingsJs.js
+++ settingsJs.js
@@ -1,11 +1,9 @@
 function saveSettings() {
   const unit = document.getElementById("tempUnit").value;
   const dark = document.getElementById("darkMode").checked;
-  const cities = document.getElementById("favCities").value;
 
   localStorage.setItem("weatie_unit", unit);
   localStorage.setItem("weatie_dark", dark);
-  localStorage.setItem("weatie_cities", cities);
 
   alert("Einstellungen gespeichert!");
 }
@@ -14,9 +12,50 @@ function saveSettings() {
 document.addEventListener("DOMContentLoaded", () => {
   const unit = localStorage.getItem("weatie_unit");
   const dark = localStorage.getItem("weatie_dark");
-  const cities = localStorage.getItem("weatie_cities");
 
   if (unit) document.getElementById("tempUnit").value = unit;
   if (dark === "true") document.getElementById("darkMode").checked = true;
   if (cities) document.getElementById("favCities").value = cities;
 });
+
+const darkToggle = document.getElementById("darkMode");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const mode = localStorage.getItem("weatie_theme");
+
+  if (mode === "light") {
+    document.body.classList.add("light-mode");
+    darkToggle.checked = false;
+  } else {
+    document.body.classList.remove("light-mode");
+    darkToggle.checked = true;
+    localStorage.setItem("weatie_theme", "dark");
+  }
+});
+
+darkToggle.addEventListener("change", () => {
+  if (darkToggle.checked) {
+    document.body.classList.remove("light-mode");
+    localStorage.setItem("weatie_theme", "dark");
+  } else {
+    document.body.classList.add("light-mode");
+    localStorage.setItem("weatie_theme", "light");
+  }
+});
+
+function frageNachStandort() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          function(position) {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            alert("Dein Standort:\nBreitengrad: " + lat + "\nLängengrad: " + lon);
+          },
+          function(error) {
+            alert("Fehler bei der Standortabfrage: " + error.message);
+          }
+        );
+      } else {
+        alert("Geolocation wird von deinem Browser nicht unterstützt.");
+      }
+    }
