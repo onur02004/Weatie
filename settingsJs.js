6479610d968134812068be51bd9d02ea10e0e048
330nur <donmezonur02004@gmail.com>
2025-06-29 21:01:02 +0200

    details Menu fast fertig, funktioniert meistens

--- settingsJs.js
+++ settingsJs.js
@@ -20,3 +20,36 @@ document.addEventListener("DOMContentLoaded", () => {
   if (dark === "true") document.getElementById("darkMode").checked = true;
   if (cities) document.getElementById("favCities").value = cities;
 });
+
+
+const toggleButton = document.getElementById('toggle-btn')
+const sidebar = document.getElementById('sidebar')
+
+function toggleSidebar() {
+  sidebar.classList.toggle('close')
+  toggleButton.classList.toggle('rotate')
+
+  closeAllSubMenus()
+}
+
+function toggleSubMenu(button) {
+
+  if (!button.nextElementSibling.classList.contains('show')) {
+    closeAllSubMenus()
+  }
+
+  button.nextElementSibling.classList.toggle('show')
+  button.classList.toggle('rotate')
+
+  if (sidebar.classList.contains('close')) {
+    sidebar.classList.toggle('close')
+    toggleButton.classList.toggle('rotate')
+  }
+}
+
+function closeAllSubMenus() {
+  Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+    ul.classList.remove('show')
+    ul.previousElementSibling.classList.remove('rotate')
+  })
+}
